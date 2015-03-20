0ebc29b23e0754e807c9d7eac60728dc0d0403b0
danbress <dbress@onyxconsults.com>
2015-03-20 14:44:32 -0400

    creating the DanBress interface and StandardDanBress implementation

---
+++ dan-api/src/main/java/danbress/com/DanBress.java
@@ -0,0 +1,20 @@
+package danbress.com;
+
+/**
+ * This is the Dan Bress interface
+ * @author Alligator
+ *
+ */
+public interface DanBress {
+    
+    /**
+     * This tells us to start something
+     */
+    void startSomething();
+    
+    /**
+     * This tells us to stop something
+     */
+    void stopSomething();
+
+}
---
+++ dan-server/src/main/java/danbress/com/StandardDanBress.java
@@ -0,0 +1,19 @@
+package danbress.com;
+
+/**
+ * This is a standard implementation of the DanBress interface.
+ * 
+ * @author Alligator
+ *
+ */
+public class StandardDanBress implements DanBress {
+
+    public void startSomething() {
+
+    }
+
+    public void stopSomething() {
+
+    }
+
+}
